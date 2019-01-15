8455faf930572e89726502def1d9811a43854285
Nick Bansal <https://github.com/NickBansal>
2019-01-15 11:58:13 +0000

    Images added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Stylesheets/App.css'
+import background from './Stylesheets/Images/background.jpg'
+import holidayData from './Data'
 
 class App extends Component {
   render() {
+    const style = {
+      backgroundImage: `url(${background})`
+    }
     return (
-      <div className="App">
-        <h1>Holiday List</h1>
+      <div style={style} className="App">
+        {console.log(holidayData)}
+        {holidayData.map((info, i) => {
+          return (
+            <div key={i} className="holidayInformation">
+              <img src={info.img} alt="holiday"/>
+            </div>
+          )
+        })}
       </div>
     );
   }
---
+++ src/Data/index.js
@@ -0,0 +1,15 @@
+import image1 from '../Stylesheets/Images/image_01.png'
+import image2 from '../Stylesheets/Images/image_02.png'
+import image3 from '../Stylesheets/Images/image_03.jpg'
+
+export default [
+    { 
+        img: image1 
+    },
+    { 
+        img: image2 
+    },
+    { 
+        img: image3 
+    }
+]
