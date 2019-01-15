d99f96d3897eca169ea9ba1479b9f4c580ab8684
Nick Bansal <https://github.com/NickBansal>
2019-01-15 20:41:06 +0000

    toggle function working

--- src/Components/HolidayInformation.jsx
+++ src/Components/HolidayInformation.jsx
@@ -2,29 +2,31 @@ import React from 'react'
 import stars from '../Stylesheets/Images/gold-stars.png'
 import '../Stylesheets/HolidayInformation.css'
 
-const HolidayInformation = ({ 
-  holidayData, toggleReverse, 
+const HolidayInformation = ({
+  holidayData, toggleReverse,
   toggleDescription, showDescription, holidayElement }) => {
 
-    const thumbnailOrder = toggleReverse ? 'column-reverse' : 'column'
-    const style = {
-        flexDirection: thumbnailOrder,
-        display: 'flex'
-    }
+  const thumbnailOrder = toggleReverse ? 'column-reverse' : 'column'
+  const style = {
+    flexDirection: thumbnailOrder,
+    display: 'flex'
+  }
 
-    const arrow = !showDescription ? 'right' : 'down'
+  const arrow = !showDescription ? 'right' : 'down'
 
-    return (<div style={style} className="FullHolidayOrder">
-         {holidayData.map((info, i) => {
-          return (
+  return (
+    <div style={style} className="FullHolidayOrder">
+      {holidayData.map((info, i) => {
+        return (
+          <div>
             <div key={i} className='HolidayInformation'>
               <img className='Holiday' src={info.img} alt='Holiday' />
               <div className='HoldiayInformationTitle'>
                 <div className='Ratings'>
-                    <p><strong>{info.title} </strong>
-                  {Array(Number(info.rating)).fill(null)
-                    .map((item, index) => <img key={index} className='StarsImage' src={stars} alt="stars" />)}
-                    </p>
+                  <p><strong>{info.title} </strong>
+                    {Array(Number(info.rating)).fill(null)
+                      .map((item, index) => <img key={index} className='StarsImage' src={stars} alt="stars" />)}
+                  </p>
                   <p>{info.location}</p>
                 </div>
                 <div className='Price'>
@@ -32,14 +34,20 @@ const HolidayInformation = ({
                   <h3>£{info.price}</h3>
                 </div>
               </div>
+
               <div className='HolidaySpecifications'>
                 <p><span>{info.date}</span> for <span>{info.days}</span> from <span>{info.airport}</span>, <span>{info.specification}</span></p>
                 <i onClick={() => toggleDescription(i)} className={`fas fa-angle-${arrow}`}></i>
-              {holidayElement === i && showDescription && <p>DESCRIPTION</p>}
               </div>
             </div>
-          )
-        })}
+            {holidayElement === i && showDescription &&
+              <div className='HolidaySpecificationsDescription'>
+                <p>DESCRIPTION</p>
+              </div>
+            }
+          </div>
+        )
+      })}
     </div>)
 }
 
