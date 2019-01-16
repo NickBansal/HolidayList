a4ef1fa3b8152eeb26ee100f0a342d3dc1231bc8
Nick Bansal <https://github.com/NickBansal>
2019-01-16 09:14:28 +0000

    Some testing added

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import '../setupTest'
+import React from 'react'
+import App from '../App'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+
+  let wrapper 
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+  it('Component matches the snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+  it('Changes the sortValue in state on click handler', () => {
+
+    wrapper.instance().handleClick('title')
+    expect(wrapper.state().sortValue).toBe('title')
+    wrapper.instance().handleClick('price')
+    expect(wrapper.state().sortValue).toBe('price')
+    wrapper.instance().handleClick('ratings')
+    expect(wrapper.state().sortValue).toBe('ratings')
+  })
+  it('Changes the toggleReverse in state on click handler', () => {
+
+    wrapper.instance().handleClick('title')
+    expect(wrapper.state().toggleReverse).toBe(false)
+    wrapper.instance().handleClick('title')
+    expect(wrapper.state().toggleReverse).toBe(true)
+    wrapper.instance().handleClick('title')
+    expect(wrapper.state().toggleReverse).toBe(false)
+    wrapper.instance().handleClick('price')
+    expect(wrapper.state().toggleReverse).toBe(false)
+ 
+  })
+  it('Changes the showDescription in state on click handler', () => {
+    wrapper.instance().toggleDescription(1)
+    expect(wrapper.state().showDescription).toBe(true)
+    expect(wrapper.state().holidayElement).toBe(1)
+    wrapper.instance().toggleDescription(2)
+    expect(wrapper.state().showDescription).toBe(false)
+    expect(wrapper.state().holidayElement).toBe(2)
+    wrapper.instance().toggleDescription(3)
+    expect(wrapper.state().showDescription).toBe(true)
+    expect(wrapper.state().holidayElement).toBe(3)
+  
+ 
+  })
+})
