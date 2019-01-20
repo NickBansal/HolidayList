6c916975e2b40fce79b3f46a617a746ff88341ea
Nick Bansal <https://github.com/NickBansal>
2019-01-20 09:45:51 +0000

    BEM classes renamed in footer

--- src/Components/ThumbnailFooter.jsx
+++ src/Components/ThumbnailFooter.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const ThumbnailFooter = ({ date, days, airport, specification, toggleDescription, _id, arrow }) => {
     return (
-        <div className='HolidaySpecifications'>
-            <p className='SpecificationParagraph'><span>{date}</span> for <span>{days}</span> from <span>{airport}</span>, <span>{specification}</span></p>
-            <button className='SpecificationToggle' onClick={() => toggleDescription(_id)}><i className={`fas fa-angle-${arrow}`}></i></button>
+        <div className='specifications'>
+            <p className='specifications__paragraph'><span>{date}</span> for <span>{days}</span> from <span>{airport}</span>, <span>{specification}</span></p>
+            <button className='.specification__toggle' onClick={() => toggleDescription(_id)}>
+            <i className={`.specification__icon fas fa-angle-${arrow}`}></i>
+            </button>
         </div>
     )
 }
