9295d0e86d99543d05c60cb464da2d3292c6106f
Nick Bansal <https://github.com/NickBansal>
2019-01-20 11:27:17 +0000

    Bold description

--- src/Components/ThumbnailFooter.jsx
+++ src/Components/ThumbnailFooter.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ThumbnailFooter = ({ date, days, airport, specification, toggleDescription, _id, arrow }) => {
     return (
         <div className='specifications'>
-            <p className='specifications__paragraph'><span>{date}</span> for <span>{days}</span> from <span>{airport}</span>, <span>{specification}</span></p>
+            <p className='specifications__paragraph'>
+            <span><strong>{date}</strong></span> for <span><strong>{days}</strong></span> from <span><strong>{airport}</strong></span>, <span><strong>{specification}</strong></span>
+            </p>
             <button className='specifications__toggle' onClick={() => toggleDescription(_id)}>
             <i className={`specifications__icon fas fa-angle-${arrow}`}></i>
             </button>
